7cc701161222258cf4c9a064fd5f096fe843c258
jamshed awan <159632135+jamshedawan@users.noreply.github.com>
2024-02-18 15:48:22 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,22 @@
-window.addEventListener("load", checkInternetConnection);
+function calculateLove() {
+    const name1 = document.getElementById("name1").value.trim();
+    const name2 = document.getElementById("name2").value.trim();
 
-function checkInternetConnection() {
-  const statusText = document.getElementById("statusText");
-  const ipAdressText = document.getElementById("IP AdresssText");
-  const NetworkStrengthText = document.getElementById("NetworkStrengthText");
+    if (name1 === "" || name2 === "") {
+        alert("Please enter both names");
+        return;
+    }
 
-  statusText.textContent = 'Checking....';
+    const loverPercentage = Math.floor(Math.random() * 101);
 
-  if (navigator.onLine) {
-    fetch( 'https://api.ipify.org?format=json')
-    .then((response) => response.json())
-    .then((data) => {
-        NetworkStrengthText.textContent = data.ip;
-        statusText.textContent = "Connected";
+    const result = document.getElementById("result");
+    result.innerHTML = `${name1} and ${name2}'s Love Percentage: ${loverPercentage}%`;
 
-        const connection = navigator.connection;
-
-        const NetworkStrength = connection
-            ? connection.downlink + "Mbps"
-            : "Unknown";
-        NetworkStrengthText.textContent = NetworkStrength;
-    })
-    .catch(() => {
-        statusText.textContent = "Disconnected";
-        ipAdressText.textContent = "-";
-        NetworkStrengthText.textContent = "-";
-    });
-
-  } else {
-    statusText.textContent = "Disconnected";
-    ipAdressText.textContent = "-";
-    NetworkStrengthText.textContent = "-";
-  }
+    if (loverPercentage < 30) {
+        result.innerHTML += "<br> Not a great match. Keep looking!";
+    } else if (loverPercentage >= 30 && loverPercentage < 70) {
+        result.innerHTML += "<br> There is potential. Give it a try!";
+    } else {
+        result.innerHTML += "<br> Great match! Love is in the air!";
+    }
 }
-
